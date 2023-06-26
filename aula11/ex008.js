6e80fa52cc93355eed19f41a3ba525b1e703de2c
JuniorWSP <juniorwender@yahoo.com.br>
2023-06-26 19:55:18 -0300

    Aula de condições if/else

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+let vel = 50
+console.log(`A velociadde do seu carro é'${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velociade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+//if = se = Condição simples
