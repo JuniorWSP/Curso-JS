37688ef69eb494732505eb48813f9c7443995b6a
JuniorWSP <juniorwender@yahoo.com.br>
2023-06-28 14:59:25 -0300

    Ajustando fotos e css

--- aula12/aula12ex-parte2/script.js
+++ aula12/aula12ex-parte2/script.js
@@ -2,21 +2,22 @@ function carregar() {
 let msg = document.getElementById('msg')
 let img = document.getElementById('imagem')
 let data = new Date()
-/* let hora = data.getHours() */
-let hora = 14
-msg.innerHTML = `Agora são ${hora} horas.`
+let hora = data.getHours()
+let minuto = data.getMinutes()
+/* let hora = 4 */
+msg.innerHTML = `Agora são ${hora}:${minuto} horas.`
 
 if (hora >= 0 && hora < 12) {
     //BOM DIA!
-    img.src = 'manha1.webp'
-    document.body.style.background = '#e9cc9f'
+    img.src = 'manha3.webp'
+    document.body.style.background = '#6f815b'
 } else if (hora >= 12 && hora <= 18) {
     //BOA TARDE!
-    img.src = 'tarde1.webp'
+    img.src = 'tarde3.webp'
     document.body.style.background = '#fe8d38'
 } else {
     //BOA NOITE!
-    img.src = 'noite1.webp'
-    document.body.style.background = '#0e0e0c'
+    img.src = 'noite3.webp'
+    document.body.style.background = '#252a30'
 }
 }
