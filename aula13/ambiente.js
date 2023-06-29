48bd7ecf53b58b57423c5deb6d23d4f29e4b0bad
JuniorWSP <juniorwender@yahoo.com.br>
2023-06-29 17:25:44 -0300

    Aula 14, for

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+let conta = 1
+let contador = 1
+
+while(conta <= 2) {
+    console.log(`'enquanto' for menor faça, passo ${conta}`)
+    conta = conta + 1
+}
+
+do {
+    console.log(`'faça' antes ate ser menor, passo ${conta}`)
+    contador = contador + 1
+} while (contador <= 3)
